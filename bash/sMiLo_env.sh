f9827548ce4c4e32c2d6d0cfc88418f192530a28
Sungjun, Kim <smilolistener@gmail.com>
2017-12-28 11:11:10 +0900

    Update sMiLo_env.sh

--- bash/sMiLo_env.sh
+++ bash/sMiLo_env.sh
@@ -5,7 +5,7 @@ alias ll='ls -alh'
 alias vi='vim -u ${HOME}/scripts/sMiLo_vimrc'
 
 # for git
-alias update_git='git fetch upstream && git merge upstream/master && git push origin master && git fetch origin --prune'
+alias update_git='git checkout master && git fetch upstream && git merge upstream/master && git push origin master && git fetch origin --prune'
 
 # for git
 export PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
